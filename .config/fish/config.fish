6e932aa7cc429b1f250c1866ed34ffa5b94a6244
Mislav Novakovic <mislav.novakovic@directconversion.com>
2022-07-06 21:38:10 +0300

    set EDITOR
    Signed-off-by: Mislav Novakovic <mislav.novakovic@directconversion.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ set fish_greeting
 
 starship init fish | source
 
+# Aliases
 alias ls=exa
 alias cat=bat
 alias find=fd
@@ -14,8 +15,11 @@ alias du=dust
 alias grep=rg
 alias zellij='zellij -l compact'
 
+# Paths
 fish_add_path $HOME/.cargo/env
 fish_add_path $HOME/.emacs.d/bin
 fish_add_path $HOME/.poetry/bin
 
+# ENV varibales
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+set -x EDITOR '/usr/bin/vim'
